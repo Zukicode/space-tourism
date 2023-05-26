133874d43ae88128ebaf4d65421f9549ec7656eb
Zukicode <zukicode@gmail.com>
2023-05-26 22:02:10 +0300

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -15,7 +15,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
 	<React.StrictMode>
-		<BrowserRouter>
+		<BrowserRouter basename='space-tourism/'>
 			<App />
 		</BrowserRouter>
 	</React.StrictMode>
